6563aded7c78be11f82248f57a8fe99c53a8fa15
kharerishabh <rishabkhare41@gmail.com>
2022-12-12 13:52:10 +0530

    My second React pro Task-8

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
+import './ExpenseItem.css';
+
 function ExpenseItem(){
-    return (<div>
-        <h2>Expense Items!</h2>
-        <div>Food Rs 10</div>
-        <div>Petrol Rs 100</div>
-        <div>Petrol Rs 100</div>
+const expenseDate = new Date(2022, 12, 12);
+const expenseTitle = 'Food';
+const expenseAmount = 100;
+const locationOfExpenditure = 'Resturant'
+
+    return (<div className='expense-item'>
+        <div>{expenseDate.toISOString()}</div>
+        <div className='expense-item__description'>
+            <h2>{expenseTitle}, {locationOfExpenditure}</h2>
+        <div className='expense-item__price'>Rs {expenseAmount}</div>  
+        </div>      
         </div>);
 }
 
