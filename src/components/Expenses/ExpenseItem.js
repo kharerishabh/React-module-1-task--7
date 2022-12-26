78b6867fbc11864ab910ce4eec946a99af5c14e1
kharerishabh <rishabkhare41@gmail.com>
2022-12-26 14:57:40 +0530

    advance react pro task-2

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,3 @@
-
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetail from './ExpenseDetail';
@@ -6,10 +5,22 @@ import ExpenseDetail from './ExpenseDetail';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) =>{
-    return (<Card className='expense-item'>
+    const clickHandler = ()=>{
+        console.log('Clicked!!')
+    }
+    const deleteHandler = ()=>{
+        const element = document.getElementById("delete");
+        //console.log(element)
+        element.remove();
+    }
+    return (<div id='delete'>
+        <Card className='expense-item'>
         <ExpenseDate date={props.date}></ExpenseDate>
         <ExpenseDetail title={props.title}amount={props.amount}location={props.location}></ExpenseDetail>
-        </Card>);
+        <button onClick={clickHandler}>Change Title</button>
+        <button onClick={deleteHandler}>Delete Expense</button>
+        </Card>
+        </div>);
 }
 
 export default ExpenseItem;
