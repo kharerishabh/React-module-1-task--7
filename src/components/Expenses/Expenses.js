a03822a071e407181c0814868c0406a66ccbc7b8
kharerishabh <rishabkhare41@gmail.com>
2022-12-29 15:21:52 +0530

    advance react pro task-11

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,20 +5,23 @@ import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-  const [filterYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense)=>{
+    return expense.date.getFullYear().toString() === filteredYear 
+  })
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter
-          selected={filterYear}
+          selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => {
+        {filteredExpenses.map((expense) => {
           return (<ExpenseItem
             key={expense.id}
             title={expense.title}
